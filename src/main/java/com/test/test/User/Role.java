5981347ef242ed73956c6defa3b837c603bd24bb
Maukan Mir <e-source@E1069.local>
2023-01-15 07:49:00 -0500

    Java project

---
+++ src/main/java/com/test/test/User/Role.java
@@ -0,0 +1,6 @@
+package com.test.test.User;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/test/test/auth/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.test.test.auth;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {public Object authenticate(AuthenticationRequest request) {
+
+    public AuthenticationResponse register(ResgisterRequest request){
+
+        return null;
+}
+
+
+}
